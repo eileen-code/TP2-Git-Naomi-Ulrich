e2aad4eb5d1dc32622c0b97436d16f96afe2d1f7
eileen-code <nefereileen@gmail.com>
2025-11-17 16:28:19 +0100

    ajout d'un message dans main sur la branche feature-message

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Projet collaboratif - TP2");
+		System.out.println("version avec nouvelle fonctionnalite");
+		
 	}
 
 }
