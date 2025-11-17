e212a402e49b615d064cce8e43a1d2838180f6b4
eileen-code <nefereileen@gmail.com>
2025-11-17 16:36:47 +0100

    deuxieme modification de la classe Main

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Projet collaboratif - TP2");
-		System.out.println("version avec nouvelle fonctionnalite");
+		System.out.println("Projet collaboratif - TP2(verion A)");
+		/*System.out.println("version avec nouvelle fonctionnalite");*/
+		
 		
 	}
 
